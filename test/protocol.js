a3222b8d0fe2285345a66823fd12bf7adc1a208d
Michael Schoonmaker <michael.r.schoonmaker@gmail.com>
2015-02-20 17:33:03 -0800

    Fix support for long message frames.

--- test/protocol.js
+++ test/protocol.js
@@ -1,9 +1,12 @@
 'use strict'
 
-var stream = require('stream')
+var fs = require('fs')
+  , path = require('path')
+  , stream = require('stream')
   , test = require('tape')
   , Builder = require('../lib/protocol/builder')
   , Parser = require('../lib/protocol/parser')
+  , LONG_FRAME = fs.readFileSync(path.join(__dirname, 'fixtures', 'long'))
 
 function createPipeline(t) {
   var vent = new stream.PassThrough({ objectMode: true })
@@ -22,14 +25,14 @@ function createPipeline(t) {
 function sendAndCheck(t, pipeline, msg) {
   var expected = Array.isArray(msg) ? msg : [msg]
 
-  validateNextChunk(t, pipeline, expected)
+  validateNextMessage(t, pipeline, expected)
   pipeline.vent.write(msg)
 }
 
-function validateNextChunk(t, pipeline, expected) {
-  t.plan(2 + 2 * expected.length)
+function validateNextMessage(t, pipeline, expected) {
+  t.plan((t._plan || 0) + (expected.length ? 2 : 0) + 2 * expected.length)
 
-  pipeline.sink.on('data', function (msg) {
+  pipeline.sink.once('data', function (msg) {
     validateData(t, msg, expected)
   })
 }
@@ -126,4 +129,94 @@ test('small, double-Buffer-element Array message', function (t) {
   sendAndCheck(t, pipeline, [new Buffer('1234'), new Buffer('5678')])
 
   pipeline.vent.end()
+})
+
+test('long String message', function (t) {
+  var pipeline = createPipeline(t)
+    , frame = LONG_FRAME
+
+  sendAndCheck(t, pipeline, String(frame))
+
+  pipeline.vent.end()
+})
+
+test('long Buffer message', function (t) {
+  var pipeline = createPipeline(t)
+    , frame = LONG_FRAME
+
+  sendAndCheck(t, pipeline, frame)
+
+  pipeline.vent.end()
+})
+
+test('long, single-String-element Array message', function (t) {
+  var pipeline = createPipeline(t)
+
+  sendAndCheck(t, pipeline, [String(LONG_FRAME)])
+
+  pipeline.vent.end()
+})
+
+test('long, single-Buffer-element Array message', function (t) {
+  var pipeline = createPipeline(t)
+
+  sendAndCheck(t, pipeline, [LONG_FRAME])
+
+  pipeline.vent.end()
+})
+
+test('long, double-String-element Array message', function (t) {
+  var pipeline = createPipeline(t)
+
+  sendAndCheck(t, pipeline, [String(LONG_FRAME), String(LONG_FRAME).toUpperCase()])
+
+  pipeline.vent.end()
+})
+
+test('long, mixed-element Array message', function (t) {
+  var pipeline = createPipeline(t)
+
+  sendAndCheck(t, pipeline, [LONG_FRAME, String(LONG_FRAME).toUpperCase()])
+
+  pipeline.vent.end()
+})
+
+test('long, double-Buffer-element Array message', function (t) {
+  var pipeline = createPipeline(t)
+
+  sendAndCheck(t, pipeline, [LONG_FRAME, new Buffer(String(LONG_FRAME).toUpperCase())])
+
+  pipeline.vent.end()
+})
+
+test('stream', function (t) {
+  var pipeline = createPipeline(t)
+
+  validateNextMessage(t, pipeline, [LONG_FRAME])
+
+  fs.createReadStream(path.join(__dirname, 'fixtures', 'long'))
+    .pipe(pipeline.vent)
+})
+
+test('kitchen sink', function (t) {
+  var pipeline = createPipeline(t)
+
+  pipeline.sink.once('data', function () {
+    sendAndCheck(t, pipeline, [LONG_FRAME, 42, '2314'])
+
+    pipeline.sink.once('data', function () {
+      sendAndCheck(t, pipeline, [1, 2, 3, 4, 5])
+
+      pipeline.sink.once('data', function () {
+        sendAndCheck(t, pipeline, [])
+
+        pipeline.sink.once('data', function () {
+          sendAndCheck(t, pipeline, ['end'])
+          pipeline.vent.end()
+        })
+      })
+    })
+  })
+
+  sendAndCheck(t, pipeline, ['start'])
 })
